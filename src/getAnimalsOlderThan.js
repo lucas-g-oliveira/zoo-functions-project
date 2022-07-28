cd83ae9a361365fc54314b339045a6c9284f76fd
Lucas G <lucasg.devmail93@gmail.com>
2022-07-28 20:50:09 -0300

    getAnimalsOlderThan -> OK

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const result = species.find((element) => element.name === animal && element.residents
+    .map((e2) => e2.age).every((agee) => agee >= age));
+  console.log(result);
+  return typeof (result) !== 'undefined';
 }
 
+console.log(getAnimalsOlderThan('bears', 5));
 module.exports = getAnimalsOlderThan;
