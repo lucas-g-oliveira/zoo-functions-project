1e8b8c0b8c5c91c762166c87599de12f90ea4bfb
Lucas G <lucasg.devmail93@gmail.com>
2022-07-29 19:54:32 -0300

    calculateEntry -> OK

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
+console.log(prices);
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const obj = { child: 0, adult: 0, senior: 0 };
+  obj.child = entrants.filter((e) => e.age < 18).length;
+  obj.adult = entrants.filter((e) => e.age >= 18 && e.age < 50).length;
+  obj.senior = entrants.filter((e) => e.age >= 50).length;
+  console.log(obj);
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  }
+  if (JSON.stringify(entrants) === '{}') {
+    return 0;
+  }
+  const obj = countEntrants(entrants);
+  obj.child *= data.prices.child;
+  obj.senior *= data.prices.senior;
+  obj.adult *= data.prices.adult;
+  return obj.child + obj.adult + obj.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
