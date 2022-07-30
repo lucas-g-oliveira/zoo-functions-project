316a7e7e3f21f40ecca2d55a4d62befab0df1b4b
Lucas G <lucasg.devmail93@gmail.com>
2022-07-30 19:47:28 -0300

    getRelatedEmployess -> OK

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,49 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
+function getIdAllsManages() {
+  const tempList = [];
+  employees.map((e) => e.managers).forEach((o) => {
+    if (o[0] !== undefined) {
+      tempList.push(o[0]);
+    }
+    if (o[1] !== undefined) {
+      tempList.push(o[1]);
+    }
+  });
+  return [...new Set(tempList)];
+}
+
+function getAllPeoplesOfMnagers() {
+  const data2 = {};
+  getIdAllsManages().forEach((e) => { data2[e] = []; });
+  employees.map((e) => {
+    if (e.managers[0] !== undefined) {
+      data2[e.managers[0]].push(`${e.firstName} ${e.lastName}`);
+    }
+    if (e.managers[1] !== undefined) {
+      data2[e.managers[1]].push(`${e.firstName} ${e.lastName}`);
+    }
+    return true;
+  });
+  return data2;
+}
+
 function isManager(id) {
-  // seu código aqui
+  return getIdAllsManages().some((e) => e === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (getAllPeoplesOfMnagers()[managerId] === undefined) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  try {
+    return getAllPeoplesOfMnagers()[managerId];
+  } catch (e) {
+    return e.mesage;
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
