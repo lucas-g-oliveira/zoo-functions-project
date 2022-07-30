1ff0f81a6a03a1a96617ecbc9121afc7b29e6e5f
Lucas G <lucasg.devmail93@gmail.com>
2022-07-30 12:09:32 -0300

    getSchedule -> OK

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,48 @@
 const data = require('../data/zoo_data');
 
+const { hours, species } = data;
+
+function daysOfTeAnimal(animal) {
+  return species.find((e) => e.name === animal).availability;
+}
+
+function animalsOfTheDay(day) {
+  const finaly = {};
+  const animalsOfDay = [];
+  const hoursDay = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+  species.forEach((e) => {
+    if (e.availability.find((i) => i === day)) {
+      animalsOfDay.push(e.name);
+    }
+  });
+  if (day !== 'Monday') {
+    finaly[day] = { officeHour: hoursDay, exhibition: animalsOfDay };
+  } else {
+    finaly[day] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  }
+  return finaly;
+}
+
+function getAllDaysHrs() {
+  const returnObject = {};
+  Object.keys(hours).forEach((e) => {
+    returnObject[e] = animalsOfTheDay(e)[e];
+  });
+  return returnObject;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const daysOfTheWeek = Object.keys(hours);
+  const allAnimals = species.map((e) => e.name);
+
+  if (allAnimals.some((e) => e === scheduleTarget)) {
+    console.log('Passou');
+    return daysOfTeAnimal(scheduleTarget);
+  }
+  if (daysOfTheWeek.some((e) => e === scheduleTarget)) {
+    return animalsOfTheDay(scheduleTarget);
+  }
+  return getAllDaysHrs();
 }
 
 module.exports = getSchedule;
